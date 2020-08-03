056bb0e67d211b85c4a17407c55299ab613b32d2
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-08-03 11:51:15 -0700

    create and modify runner model

--- config/passport.js
+++ config/passport.js
@@ -1,1 +1,12 @@
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const Runner = require('../models/runner');
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+},
+function(accessToken, refreshToken, profile, cb) {
+
+}
+));
---
+++ models/runner.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const runnerSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    avatarURL: String,
+    googleId: String
+}, { 
+    timestamps: true 
+});
+
+module.exports = mongoose.model('Runner', runnerSchema);
