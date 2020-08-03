b8ec2d0e03e24078ac52e6abdd7531ba90e9fadc
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-08-03 11:01:23 -0700

    database renamed plans

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/movies', {
+mongoose.connect('mongodb://localhost/plans', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
