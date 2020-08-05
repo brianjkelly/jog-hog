efaaa0f0de9d59751104365a3b36aa2389d229a5
Brian Kelly <briankelly@Brians-Mac-mini.local>
2020-08-05 14:57:00 -0700

    create action for entries

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,12 +1,23 @@
 const Journal = require('../models/journal');
 
 module.exports = {
-    new: newEntry
+    new: newEntry,
+    create
 };
 
 function newEntry(req, res) {
     res.render('entries/new', {
         title: 'Create a New Entry for Journal',
         journalId: req.params.id
+    });
+};
+
+function create(req, res) {
+    req.body.journal = req.params.id
+    Journal.findById(req.params.id, function(err, journal) {
+        journal.entries.push(req.body);
+        journal.save(function(err) {
+            res.redirect(`journals/${journal._id}`)
+        });
     });
 };
--- routes/entries.js
+++ routes/entries.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const entriesCtrl = require('../controllers/entries');
 
 router.get('/journals/:id/entries/new', entriesCtrl.new);
+router.post('/journals/:id/entries', entriesCtrl.create);
 
 module.exports = router;
